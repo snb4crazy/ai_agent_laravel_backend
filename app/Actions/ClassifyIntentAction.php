6e80f6588fbde04750cb4a5ad95ec45bb8a45aca
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-01 19:31:13 -0400

    Refactoring Actions: namespace ClassifyIntentAction

--- app/Actions/ClassifyIntentAction.php
+++ app/Actions/ClassifyIntentAction.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Actions\Stubs;
+namespace App\Actions;
 
-use App\Actions\Contracts\ActionStubInterface;
+use App\Actions\Contracts\ActionInterface;
 
-class ClassifyIntentActionStub implements ActionStubInterface
+class ClassifyIntentAction implements ActionInterface
 {
     public function name(): string
     {
