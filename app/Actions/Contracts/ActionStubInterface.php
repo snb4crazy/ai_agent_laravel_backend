9055d9f0ee32cbab90f18912b26e36370a0ba1d3
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-01 19:34:50 -0400

    Refactoring Actions: SummarizeTextAction

--- app/Actions/Contracts/ActionStubInterface.php
+++ app/Actions/Contracts/ActionStubInterface.php
@@ -2,13 +2,7 @@
 
 namespace App\Actions\Contracts;
 
-interface ActionStubInterface
-{
-    public function name(): string;
-
-    /**
-     * @param  array<string, mixed>  $input
-     * @return array<string, mixed>
-     */
-    public function handle(array $input): array;
-}
+/**
+ * @deprecated Use ActionInterface instead.
+ */
+interface ActionStubInterface extends ActionInterface {}
